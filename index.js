95cf75ca76aae6708e9792b93a4859a17d0a97ac
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2023-06-28 22:16:52 +0530

    fifth-commit

--- index.js
+++ index.js
@@ -6,35 +6,35 @@ $(document).ready(function() {
             var c = (f - 32) * (5 / 9);
             var k = c + 273.15;
             var r = f + 459.67;
-            $('#Celsius').val(c.toFixed(4));
-            $("#Kelvin").val(k.toFixed(4));
-            $("#Rankine").val(r.toFixed(4));
+            $('#Celsius').val(parseFloat(c.toFixed(4)));
+            $("#Kelvin").val(parseFloat(k.toFixed(4)));
+            $("#Rankine").val(parseFloat(r.toFixed(4)));
         } else if ($("#Celsius").val()) {
             var c = Number($("#Celsius").val());
             var f = (c * (9 / 5)) + 32;
             var k = c + 273.15;
             var r = f + 459.67;
-            $('#Fahrenheit').val(f.toFixed(4));
-            $("#Kelvin").val(k.toFixed(4));
-            $("#Rankine").val(r.toFixed(4));
+            $('#Fahrenheit').val(parseFloat(f.toFixed(4)));
+            $("#Kelvin").val(parseFloat(k.toFixed(4)));
+            $("#Rankine").val(parseFloat(r.toFixed(4)));
 
         } else if ($("#Kelvin").val()) {
             var k = Number($("#Kelvin").val());
             var c = k - 273.15;
             var f = (c * (9 / 5)) + 32;
             var r = f + 459.67;
-            $('#Celsius').val(c.toFixed(4));
-            $("#Fahrenheit").val(f.toFixed(4));
-            $("#Rankine").val(r.toFixed(4));
+            $('#Celsius').val(parseFloat(c.toFixed(4)));
+            $("#Fahrenheit").val(parseFloat(f.toFixed(4)));
+            $("#Rankine").val(parseFloat(r.toFixed(4)));
 
         } else if ($("#Rankine").val()) {
             var r = Number($("#Rankine").val());
             var f = r - 459.67;
             var c = (f - 32) * (5 / 9);
             var k = c + 273.15;
-            $("#Fahrenheit").val(f.toFixed(4));
-            $('#Celsius').val(c.toFixed(4));
-            $("#Kelvin").val(k.toFixed(4));
+            $("#Fahrenheit").val(parseFloat(f.toFixed(4)));
+            $('#Celsius').val(parseFloat(c.toFixed(4)));
+            $("#Kelvin").val(parseFloat(k.toFixed(4)));
         } else {
             alert("Please Enter a Number to Convert");
         }
